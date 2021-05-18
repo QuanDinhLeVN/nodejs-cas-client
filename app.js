fabb5c149044db9bc25899493a9dd05c7fa1f6e5
quanledinh <quan.levictoldq38@hcmut.edu.vn>
2021-05-19 02:40:01 +0700

    add FE, update route

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const session = require('express-session');
 const CASAuthentication = require('node-cas-authentication');
-
+const exphbs = require('express-handlebars');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,7 +13,8 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.engine('hbs', exphbs({extname: '.hbs'}));
+//app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
@@ -47,9 +48,45 @@ app.use( session({
 //app.use('/users', usersRouter);
 
 
-app.get( '/app', cas.bounce, function ( req, res ) {
+app.use(function(req, res, next){
+  if(req.session[ cas.session_name ]){
+    res.locals.isAuthenticated = true;
+    res.locals.username = req.session[ cas.session_name ];
+  }
+  next();
+})
+
+app.get('', (req,res)=>{
+  res.render('home');
+})
+
+
+app.get( '/secret', cas.bounce, function ( req, res ) { 
+  res.render('secret', )
+});
+
+app.get( '/app1', function ( req, res ) {
   res.send( '<html><body>Hello!</body></html>' );
 });
+
+// app.post( '/logout', cas.logout, (req, res)=>{
+//   res.locals.isAuthenticated = false;
+//   res.redirect('/')
+// });
+app.get( '/login', cas.bounce, (req, res)=>{
+
+  res.redirect("/")
+});
+app.post( '/logout', cas.logout);
+
+
+
+app.get( '/api/user', cas.block, function ( req, res ) {
+  console.log(req.session);
+  res.json( { cas_user: req.session[ cas.session_name ] } );
+});
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
